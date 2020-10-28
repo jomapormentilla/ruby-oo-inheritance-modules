7eaedc7e26337068fb09c47bc1640bea83955b34
jomapormentilla <jomapormentilla@gmail.com>
2020-10-28 13:49:51 -0400

    commit tree ba5e938ea4b902c0c9960d9809f160aafb173b86

---
+++ lib/class_methods_module.rb
@@ -0,0 +1,7 @@
+module MetaDancing
+    # Methods intended to be used as Class Methods in the Kid and Dancer classes
+    def metadata
+      "This class produces objects that love to dance."
+    end
+
+  end 
